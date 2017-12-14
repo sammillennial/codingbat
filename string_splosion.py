d86d0856897c957c0e7b5d59db6228f8d3823ce3
sammillennial <shawkins6@twu.edu>
2017-12-14 17:00:11 -0600

    Create string_splosion.py

---
+++ string_splosion.py
@@ -0,0 +1,11 @@
+# Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+# string_splosion('Code') → 'CCoCodCode'
+# string_splosion('abc') → 'aababc'
+# string_splosion('ab') → 'aab'
+
+def string_splosion(str):
+  y = ''
+  for i in range(len(str)):
+    y = y + str[:i+1]
+  return y
