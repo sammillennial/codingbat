ed2b9fdd2be2a9053a8d1f2e15907bc647a5903b
sammillennial <shawkins6@twu.edu>
2017-12-18 14:59:29 -0600

    Create count_hi.py

---
+++ count_hi.py
@@ -0,0 +1,10 @@
+# Return the number of times that the string "hi" appears anywhere in the given string.
+
+# count_hi('abc hi ho') → 1
+# count_hi('ABChi hi') → 2
+# count_hi('hihi') → 2
+
+def count_hi(str):
+  hi = 'hi'
+  count = 0
+  return str.count(hi)
