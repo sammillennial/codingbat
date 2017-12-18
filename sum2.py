a00f328e83561962c1f28baceac77126f853a406
sammillennial <shawkins6@twu.edu>
2017-12-18 12:39:40 -0600

    Create sum2.py

---
+++ sum2.py
@@ -0,0 +1,11 @@
+# Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+
+# sum2([1, 2, 3]) → 3
+# sum2([1, 1]) → 2
+# sum2([1, 1, 1, 1]) → 2
+
+def sum2(nums):
+  x = sum(nums)
+  if len(nums) > 2:
+    x = nums[0] + nums[1]
+  return x
