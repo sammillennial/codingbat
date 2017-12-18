d634d0f9a955848b95d7b976034520c6759d4dd3
sammillennial <shawkins6@twu.edu>
2017-12-18 14:56:46 -0600

    Create make_chocolate.py

---
+++ make_chocolate.py
@@ -0,0 +1,11 @@
+# We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each). Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
+
+# make_chocolate(4, 1, 9) → 4
+# make_chocolate(4, 1, 10) → -1
+# make_chocolate(4, 1, 7) → 2
+
+def make_chocolate(small, big, goal):
+  #big_total = big*5
+  big_min = min(big, goal//5)
+  little_need = abs(goal - big_min*5)
+  return little_need if little_need<=small else -1
